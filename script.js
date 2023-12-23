554f91fa12ea4f26cc13d89b2b8be829d4a6df16
Vighanesh Gavit <vighaneshkapil@gmail.com>
2023-12-23 17:08:50 +0530

    Changes in index and style files

---
+++ script.js
@@ -0,0 +1,8 @@
+// Hamburger Section
+$('#menu-btn').click(function() {
+    $('.header .navigation .navbar-list').addClass('active')
+});
+
+$('#menu-close').click(function() {
+    $('.header .navigation .navbar-list').removeClass('active')
+});
